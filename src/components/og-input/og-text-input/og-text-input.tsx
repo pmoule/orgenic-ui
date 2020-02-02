e365056232ef89ac4e32b4d0550a0c1d07e99789
dev@patrickmoule.net <dev@patrickmoule.net>
2020-02-02 17:50:42 +0100

    Squashed commit of the following:
    commit a8e692e688771fea34f87119f37a71f659acf4e0
    Author: dev@patrickmoule.net <dev@patrickmoule.net>
    Date:   Sun Feb 2 17:48:38 2020 +0100
    
        fix: fix for earlier stencil version
    
    commit e73722ee7e434324ab612252ffbf74061c355c73
    Author: dev@patrickmoule.net <dev@patrickmoule.net>
    Date:   Thu Jan 16 21:48:34 2020 +0100
    
        added autofocus to text input

--- src/components/og-input/og-text-input/og-text-input.tsx
+++ src/components/og-input/og-text-input/og-text-input.tsx
@@ -6,11 +6,19 @@ import { h, Component, Prop, Event, EventEmitter, Host } from '@stencil/core';
   shadow: true
 })
 export class OgTextInput {
+  private textInput?: HTMLInputElement;
+
   /**
    * Optional placeholder text if input is empty.
    */
   @Prop()
   public placeholder?: string;
+
+  /**
+   * Autofocus component when set.
+   */
+  @Prop({ mutable: false, reflect: false })
+  public autofocus: boolean;
 
   /**
    * The initial value. Can be updated at runtime.
@@ -47,10 +55,28 @@ export class OgTextInput {
     this.valueChanged.emit(this.value);
   }
 
+  private focus: boolean = false;
+
+  componentWillLoad() {
+    if (this.autofocus) {
+      this.focus = true;
+    } 
+  }
+
+  componentDidLoad() {
+    if (this.autofocus && this.focus) {
+      setTimeout(() => {
+        this.textInput.focus();
+        this.focus = false;
+      });
+    } 
+  }
+
   public render(): HTMLElement {
     return (
       <Host class={{ 'og-form-item__editor': true }}>
         <input type="text"
+          ref={ el => this.textInput = el as HTMLInputElement }
           class="og-input__input"
           value={ this.value }
           disabled={ this.disabled }
